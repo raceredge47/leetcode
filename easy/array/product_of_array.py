184f3bd25f5af509318088c8c595cc7e53167ab8
raceredge47 <amansrivastav.bytequests@gmail.com>
2025-10-06 16:55:09 +0530

    Product of Array

---
+++ easy/array/product_of_array.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        product = 1
+        for i in nums:
+            product = product * i
+        if product > 0:
+            return 1
+        if product == 0:
+            return 0
+        if product < 0:
+            return -1
